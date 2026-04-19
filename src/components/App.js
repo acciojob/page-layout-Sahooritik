df299db4b27040df38efd0613dd96eb53f429b24
Ritik Sahoo <ritiksahoo380@gmail.com>
2026-04-19 18:00:06 +0530

    v2

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
 import React from "react";
 import './../styles/App.css';
+import PageLayout from "./PageLayout";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <PageLayout
+         header="Welcome to my website"
+         children="This is the content of my website."
+         footer ="&copy; 2023 My Website. All rights reserved."
+        />
     </div>
   )
 }
